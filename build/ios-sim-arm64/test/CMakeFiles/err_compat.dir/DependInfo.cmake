aa604841e41bc6998a2b49a0fbbb32dbebd4e7c6
Stefano Rizzetto <stefano@airbagstudio.it>
2025-04-08 17:11:06 +0200

    static library

---
+++ build/ios-sim-arm64/test/CMakeFiles/err_compat.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/stefanorizzetto/Documents/HDF5Kit/hdf5-1.14.6/test/err_compat.c" "test/CMakeFiles/err_compat.dir/err_compat.c.o" "gcc" "test/CMakeFiles/err_compat.dir/err_compat.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
